faf3f8e373c758695d60622199fe25e7b741027a
Ranjitrajak <mail4ranjitrajak@gmail.com>
2022-05-26 11:46:21 +0530

    new

--- src/routes/route.js
+++ src/routes/route.js
@@ -9,12 +9,13 @@ router.post("/register",createUser)
 router.post("/login", loginUser)
 
 router.get("/user/:userId/profile",authentication, getUserProfile)
-router.post("/update/:userId",updateUser)
+//router.post("/update/:userId",updateUser)
+router.put("/user/:userId/profile",updateUser )
 router.post("/products",createProduct)
 router.get("/products",getproducts)
 router.get("products/:productId",getProductById)
 
-router.put("/user/:userId/profile",authentication, authorisation, updateUser )
+
 
 
 module.exports =router
